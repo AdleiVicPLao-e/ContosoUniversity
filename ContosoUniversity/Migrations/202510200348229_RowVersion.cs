cb1892c0e48dbd2be7caf9ab9fe931a7a6c70150
aplaoe <aplaoe@SSCLTRAPLAOE>
2025-10-20 13:50:35 +0800

    Add project files.

---
+++ ContosoUniversity/Migrations/202510200348229_RowVersion.cs
@@ -0,0 +1,26 @@
+﻿namespace ContosoUniversity.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class RowVersion : DbMigration
+    {
+        public override void Up()
+        {
+            DropColumn("dbo.Course", "RowVersion");
+            DropColumn("dbo.Instructor", "RowVersion");
+            DropColumn("dbo.OfficeAssignment", "RowVersion");
+            DropColumn("dbo.Enrollment", "RowVersion");
+            DropColumn("dbo.Student", "RowVersion");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Student", "RowVersion", c => c.Binary(nullable: false, fixedLength: true, timestamp: true, storeType: "rowversion"));
+            AddColumn("dbo.Enrollment", "RowVersion", c => c.Binary(nullable: false, fixedLength: true, timestamp: true, storeType: "rowversion"));
+            AddColumn("dbo.OfficeAssignment", "RowVersion", c => c.Binary(nullable: false, fixedLength: true, timestamp: true, storeType: "rowversion"));
+            AddColumn("dbo.Instructor", "RowVersion", c => c.Binary(nullable: false, fixedLength: true, timestamp: true, storeType: "rowversion"));
+            AddColumn("dbo.Course", "RowVersion", c => c.Binary(nullable: false, fixedLength: true, timestamp: true, storeType: "rowversion"));
+        }
+    }
+}
